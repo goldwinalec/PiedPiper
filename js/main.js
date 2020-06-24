d428a0671254e6cefb31570017a69c9243eb7789
Alina Gladkova <a-g1997@yandex.ru>
2020-06-24 18:12:01 +0300

    Back to top btn

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ $(document).ready(function () {
     $(".header__nav").toggleClass("active");
     $("body").toggleClass("no-scroll");
   });
+  $(".top-btn").removeClass("visible");
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".top-btn").addClass("visible");
+    } else {
+      $(".top-btn").removeClass("visible");
+    }
+  });
+
+  $(".top-btn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
 });
